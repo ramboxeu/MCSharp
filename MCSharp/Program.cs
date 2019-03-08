7caf80fe7c981a9842e0be369fe30a70d4ff8fba
RamboxEu <mail.rambox@gmail.com>
2019-03-08 21:31:03 +0100

    Project setup

---
+++ MCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MCSharp
+Custom programming language that makes Minecraft Functions sharper.
